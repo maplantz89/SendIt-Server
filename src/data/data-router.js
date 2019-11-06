81ef3dd997d0c87dc55815c4a6a665bdfccdcabd
Martha Plantz <maplantz89@gmail.com>
2019-11-06 14:10:04 -0800

    working towards services for projects and attempts per difficulty

--- src/data/data-router.js
+++ src/data/data-router.js
@@ -26,25 +26,19 @@ dataRouter
   });
 
 dataRouter
-  .route('/:user_id/total_projects/:difficulty')
+  .route('/:user_id/total_projects/')
   .get((req, res, next) => {
-    const { user_id, difficulty } = req.params;
-    DataService.getTotalProjectsPerDifficulty(req.app.get('db'), user_id, difficulty)
-      .then(result => {
-        return res.json(result);
-      })
-      .catch(next);
-  });
-
-dataRouter
-  .route('/:user_id/attempts_per_difficulty/:difficulty')
-  .get((req, res, next) => {
-    const { user_id, difficulty } = req.params;
-    DataService.getAttemptsPerDifficulty(req.app.get('db'), user_id, difficulty)
-      .then(result => {
-        return res.json(result);
-      })
-      .catch(next);
+    const { user_id } = req.params;
+    let projects = [];
+    for(let i = 0; i<11; i++){
+      DataService.getProjectsPerDifficulty(req.app.get('db'), user_id, i)
+        .then(result => {
+          projects.push(result[0]);
+          console.log('projects', projects);
+        });
+    }
+    console.log('projects', projects);
+    return projects;
   });
 
 module.exports = dataRouter;
--- src/data/data-service.js
+++ src/data/data-service.js
@@ -13,22 +13,12 @@ const DataService = {
       .sum('attempts As attempts')
       .first();
   },
-  getTotalProjectsPerDifficulty(db, user_id, difficulty){
+  getProjectsPerDifficulty(db, user_id, difficulty){
     return db
       .from('climbs')
       .where({user_id: user_id, difficulty: difficulty})
-      .as('projects')
-      .count('attempts As projects_per_difficulty')
-      .first();
+      .count('difficulty As ' + difficulty);
   },
-  getAttemptsPerDifficulty(db, user_id, difficulty){
-    return db
-      .from('climbs')
-      .where({user_id: user_id, difficulty: difficulty})
-      .as('projects')
-      .sum('attempts As attempts_per_difficulty')
-      .first();
-  }
 };
 
 module.exports = DataService;
